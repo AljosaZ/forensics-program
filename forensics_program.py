3eb2d847d41984a3fec89b5df5f775a5cfe3ff2f
AljosaZ <znidaric.aljosa@gmail.com>
2020-05-17 15:21:30 +0200

    replaced counter with True/False logic

--- forensics_program.py
+++ forensics_program.py
@@ -1,20 +1,24 @@
-#This is a solution to the problem on https://github.com/smartninja/wd1-py3-exercises/tree/master/lesson-11/CSI.
+# #This is a solution to the problem on https://github.com/smartninja/wd1-py3-exercises/tree/master/lesson-11/CSI.
 
 
-suspects = {"Eva": ["TGAAGGACCTTC", "AAAACCTCA", "TTAGCTATCGC", "TTGTGGTGGC", "AGGCCTCA"],
-          "Larisa": ["TGAAGGACCTTC", "AAAACCTCA", "GCCAGTGCCG", "AAGTAGTGAC", "AGGCCTCA"],
-          "Matej": ["TGCAGGAACTTC", "AAAACCTCA", "CCAGCAATCGC", "TTGTGGTGGC", "AGGCCTCA"],
-          "Miha": ["TGCAGGAACTTC", "AAAACCTCA", "GCCAGTGCCG", "GGGAGGTGGC", "GCCACGG"]
-          }
+suspects = {
+    "Eva": ["TGAAGGACCTTC", "AAAACCTCA", "TTAGCTATCGC", "TTGTGGTGGC", "AGGCCTCA"],
+    "Larisa": ["TGAAGGACCTTC", "AAAACCTCA", "GCCAGTGCCG", "AAGTAGTGAC", "AGGCCTCA"],
+    "Matej": ["TGCAGGAACTTC", "AAAACCTCA", "CCAGCAATCGC", "TTGTGGTGGC", "AGGCCTCA"],
+    "Miha": ["TGCAGGAACTTC", "AAAACCTCA", "GCCAGTGCCG", "GGGAGGTGGC", "GCCACGG"]
+}
 
 
-with open('dna.txt', 'r') as dna_sequence:
+with open("dna.txt", "r") as dna_sequence:
     dna_string = dna_sequence.read()
 
+
 for suspect in suspects:
-    counter = 0
     for char in suspects[suspect]:
+        found = False
         if char in dna_string:
-            counter += 1
-            if counter == 5:
-                print(f"We found the suspect. The ice cream thief was {suspect} all along!")
+            found = True
+        else:
+            break
+    if found:
+        print(f"We found the suspect. The ice cream thief was {suspect} all along!")
